ea1f0db871ce3394643b95b2a7893ae50b8f672f
Tom <tom@Toms-MacBook-Pro.local>
2025-08-26 17:03:59 +1000

    created adding function

--- c_src/src/matrix.c
+++ c_src/src/matrix.c
@@ -22,7 +22,18 @@ void free_matrix(double** matrix, int rows) {
 }
 
 // Operations
-double** matrix_add(double** A, double** B, int rows, int cols);
+double** matrix_add(double** matrix1, double** matrix2, int rows, int cols) {
+  double** added_matrix = create_matrix(rows, cols);
+
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < cols; j++) {
+      added_matrix[i][j] = matrix1[i][j] + matrix2[i][j];
+    }
+  } 
+
+  return added_matrix;
+}
+
 double** matrix_sub(double** A, double** B, int rows, int cols);
 double** matrix_multiply(double** A, double** B, int a_rows, int a_cols, int b_cols);
 double** matrix_transpose(double** A, int rows, int cols);
