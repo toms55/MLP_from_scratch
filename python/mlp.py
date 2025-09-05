86cfa268b8965611352b1e064663c5a1203418df
Tom <tom@Toms-MacBook-Pro.local>
2025-09-06 09:56:18 +1000

    began fleshing out mlp.py

--- python/mlp.py
+++ python/mlp.py
@@ -1,1 +1,9 @@
-import c_wrapper
+from . import c_wrapper
+import numpy as np
+
+class MLP:
+    def __init__(self, layer_sizes, activation="ReLu", loss="MSE", learning_rate=0.01):
+        self.layer_sizes = layer_sizes
+        self.activation = activation
+        self.loss = loss
+        self.learning_rate = learning_rate
